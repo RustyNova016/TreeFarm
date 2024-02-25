f91471c11a55154ee8b9f912040f6bb045a875a5
RustyNova016 <50844553+RustyNova016@users.noreply.github.com>
2024-02-25 12:19:34 +0000

    control installer

---
+++ install_control.lua
@@ -0,0 +1,6 @@
+os.run("rm control.lua")
+os.run("wget https://raw.githubusercontent.com/RustyNova016/TreeFarm/master/control.lua")
+
+os.run("rm lain.lua")
+os.run("wget https://raw.githubusercontent.com/RustyNova016/TreeFarm/master/lain.lua")
+os.run("control.lua")
