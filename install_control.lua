06698309d02dc9d6e19bd4c5b92c345e057d05d8
RustyNova016 <50844553+RustyNova016@users.noreply.github.com>
2024-02-25 12:21:51 +0000

    Fixed Run

--- install_control.lua
+++ install_control.lua
@@ -1,6 +1,10 @@
-os.run("rm control.lua")
-os.run("wget https://raw.githubusercontent.com/RustyNova016/TreeFarm/master/control.lua")
+-- Get control script
+os.run({}, "rm control.lua")
+os.run({}, "wget https://raw.githubusercontent.com/RustyNova016/TreeFarm/master/control.lua")
 
-os.run("rm lain.lua")
-os.run("wget https://raw.githubusercontent.com/RustyNova016/TreeFarm/master/lain.lua")
-os.run("control.lua")
+-- Get lain lib script
+os.run({}, "rm lain.lua")
+os.run({}, "wget https://raw.githubusercontent.com/RustyNova016/TreeFarm/master/lain.lua")
+
+-- Run Control's setup
+os.run({}, "control.lua")
